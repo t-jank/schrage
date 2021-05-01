ed1fcd8828e43de48bfc24bc09b423afb9f34a1e
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2021-05-01 21:46:41 +0200

    bug fixed, awesome Schrage z podzialem algorithm

--- schrage.cpp
+++ schrage.cpp
@@ -50,15 +50,13 @@ void Schrage(int n,int*R,int*P,int*Q,int*X) // dane wejsciowe, X - proponowana k
     }
 }
 
-int Schrage_zPodzialem(int n,int*R,int*P,int*Q,int*X)
+int Schrage_zPodzialem(int n,int*R,int*P,int*Q)
 {
     int A[100],K[100];
     int a=n,k=0,x=0,t=0,p=0,l=-1,cmaks=0; // p - czas wykonywania zadania przed przerwaniem; l - wykonywane zadanie
     for(int i=0;i<n;i++) A[i]=i;
     for(int i=0;i<(n-1);i++) for(int j=0;j<(n-1);j++) if(R[A[j]]<R[A[j+1]]) swap(A[j],A[j+1]); // sort r
     while(a!=0 || k!=0){ // kiedy sa jeszcze zadania niedojechane lub w kolejce
-  //      cout << "cmaks: " << cmaks << endl;
-   //     cout << "t: " << t << endl;
         if(a!=0){
             if(R[A[a-1]]<=t){
                 K[k]=A[a-1];
@@ -67,36 +65,34 @@ int Schrage_zPodzialem(int n,int*R,int*P,int*Q,int*X)
                     if( Q[K[e]] < Q[K[e-1]] )
                         swap(K[e], K[e-1]);
                 }
-                if( l!=-1 && Q[K[k-1]] > Q[l] ) // sprawdzamy czy robimy przerwanie
-                {
-                    K[k]=l;
-                    k++;
-                    swap(K[k-1],K[k-2]);
-               //     P[K[k-2]] = P[K[k-2]] - p;
-                    l=-1;
-                }
+                if(l!=-1)
+                    if( Q[K[k-1]] > Q[l] ){ // sprawdzamy czy robimy przerwanie
+                        K[k]=l;
+                        k++;
+                        swap(K[k-1],K[k-2]);
+                        l=-1;
+                    }
                 continue;
             }
         }
-        if(k!=0)
-        {
-            l=K[k-1];
-       //     X[x]=K[k-1];
-            k--; x++;
-            if(a!=0) {p = min( P[l], R[a-1]-t ); cout << "t: " << t << " a: " <<a<< " R[a-1]: "<<R[a-1] <<endl;}
+        if(k!=0){
+            if(-1==l){
+                l=K[k-1];
+                k--;
+            }
+            if(a!=0) p = min( P[l], R[A[a-1]]-t );
             else p=P[l];
             t = t + p; // leci czas
             P[l] = P[l] - p;
             if(0==P[l]){
-                l=-1;
                 cmaks=max(cmaks,t+Q[l]);
+                l=-1;
             }
             continue;
         }
         if(0==k && a!=0)
             if(R[A[a-1]]>t)
                 t=R[A[a-1]];
-
     }
     return cmaks;
 }
@@ -113,14 +109,12 @@ int main()
         plik >> R[i] >> P[i] >> Q[i];
     plik.close();
 
-   /* Schrage(n,R,P,Q,X);
-    cout << "\nKolejnosc:\n"; for(int i=0;i<n;i++) cout << X[i]+1 << " ";
-    cout << "\nCmax: " << cmax(n,R,P,Q,X) << endl << endl;*/
+    Schrage(n,R,P,Q,X);
+    cout << "schr:\n" << cmax(n,R,P,Q,X) << endl;
+    for(int i=0;i<n;i++) cout << X[i]+1 << " ";
 
- //   cout << "schrpmtn = " << Schrage_zPodzialem(n,R,P,Q,X) << endl;
-    Schrage_zPodzialem(n,R,P,Q,X);
+    cout << "\n\nschrpmtn = " << Schrage_zPodzialem(n,R,P,Q) << endl;
 
     cin.get();
-
     return 0;
 }
